b4ac656d8200522dcc9905a1c1e565915510da1a
FlezZz23 <76622445+FlezZz23@users.noreply.github.com>
2020-12-29 13:19:28 +0300

    Update Vector.cpp

--- VECTOR/VECTOR/Vector.cpp
+++ VECTOR/VECTOR/Vector.cpp
@@ -4,19 +4,19 @@ Vector::Vector()
 {
 	x = 0;
 	y = 0;
-	z = 0;
+	//z = 0;
 }
-Vector::Vector(float a, float b, float c)
+Vector::Vector(float a, float b)
 {
 	x = a;
 	y = b;
-	z = c;
+//	z = c;
 }
 Vector::Vector(const Vector& a) // конструктор копирования
 {
 	x = a.x;
 	y = a.y;
-	z = a.z;
+//	z = a.z;
 }
 Vector& Vector:: operator = (const Vector& a) // операция присваивания
 {
@@ -24,7 +24,7 @@ Vector& Vector:: operator = (const Vector& a) // операция присваивания
 		return *this;
 	x = a.x;
 	y = a.y;
-	z = a.z;
+//	z = a.z;
 	return *this;
 }
 Vector Vector::operator+ (Vector a)
@@ -32,7 +32,7 @@ Vector Vector::operator+ (Vector a)
 	Vector c;
 	c.x = x + a.x;
 	c.y = y + a.y;
-	c.z = z + a.z;
+	//c.z = z + a.z;
 	return c;
 }
 Vector Vector::operator- (Vector a)
@@ -40,7 +40,7 @@ Vector Vector::operator- (Vector a)
 	Vector c;
 	c.x = x - a.x;
 	c.y = y - a.y;
-	c.z = z - a.z;
+//	c.z = z - a.z;
 	return c;
 }
 Vector Vector::operator* ( float b)
@@ -48,29 +48,29 @@ Vector Vector::operator* ( float b)
 	Vector c;
 	c.x = x * b;
 	c.y = y * b;
-	c.z = z * b;
+//	c.z = z * b;
 	return c;
 }
 void Vector::print()
 {
-	cout << "(" << x << " , " << y << " , " << z << ")" << endl;
+	cout << "(" << x << " , " << y  << ")" << endl;
 }
 
 
-void  get_coordinates(Vector f,float& a, float& b, float& c) {
+void  get_coordinates(Vector f,float& a, float& b) { //, float& c) {
 	a = f.x;
 	b = f.y;
-	c = f.z;
+//	c = f.z;
 }
 float skal(Vector a, Vector b)
 {
-	return a.x * b.x + a.y * b.y + a.z * b.z;
+	return a.x * b.x + a.y * b.y; // +a.z * b.z;
 }
-Vector vect(Vector a, Vector b)
-{
-	Vector c;
-	c.x = a.y * b.z - a.z * b.y;
-	c.y = a.x * b.z - a.z * b.y;
-	c.z = a.x * b.y - a.y * b.x;
-	return c;
-}
+//Vector vect(Vector a, Vector b)
+//{
+//	Vector c;
+//	c.x = a.y * b.z - a.z * b.y;
+//	c.y = a.x * b.z - a.z * b.y;
+//	c.z = a.x * b.y - a.y * b.x;
+//	return c;
+//}
